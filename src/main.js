0fcdfcbf14ccfc3e25963c17c3044c90a6e8fe08
Jay Lee <jayin920805@gmail.com>
2025-05-03 12:04:54 +0800

    Add Google Analytics tracking with vue-gtag integration

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
+import { createGtag } from "vue-gtag";
 import App from './App.vue'
 import router from './router'
 
+const gtag = createGtag({
+    tagId: "G-9B0Q6G79HH"
+})
 // Create app instance
 const app = createApp(App)
 
 // Use plugins
 app.use(router)
+app.use(gtag)
 
 // Mount the app
 app.mount('#app')
